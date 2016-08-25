38f0409aae51b548f4512b35d944883ff8d3c5c6
AgaramDevelopment <apple@apples-macbook-pro-7.local>
2016-08-25 18:27:03 +0530

    Add custom cell

---
+++ Yazaki/DashBoardCell.h
@@ -0,0 +1,16 @@
+//
+//  DashBoardCell.h
+//  Yazaki
+//
+//  Created by APPLE on 25/08/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashBoardCell : UICollectionViewCell
+
+@property (nonatomic,strong) IBOutlet UIImageView * icon_Img;
+@property (nonatomic,strong) IBOutlet UILabel *iconName_lbl;
+
+@end
---
+++ Yazaki/DashBoardCell.m
@@ -0,0 +1,18 @@
+//
+//  DashBoardCell.m
+//  Yazaki
+//
+//  Created by APPLE on 25/08/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#import "DashBoardCell.h"
+
+@implementation DashBoardCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
